4155e0fba2052243677e21243888bc197b2677a8
Maximo Dip <maxcodingg@gmail.com>
2024-05-19 07:15:50 -0300

    idkkkk

---
+++ src/lib/index.ts
@@ -0,0 +1,23 @@
+export const cocinaImages = [
+  {
+    image: "/images/cocina/1.jpg",
+  },
+  {
+    image: "/images/cocina/2.jpg",
+  },
+  {
+    image: "/images/cocina/3.jpg",
+  },
+  {
+    image: "/images/cocina/4.jpg",
+  },
+  {
+    image: "/images/cocina/5.jpg",
+  },
+  {
+    image: "/images/cocina/6.jpg",
+  },
+  {
+    image: "/images/cocina/7.jpg",
+  },
+];
